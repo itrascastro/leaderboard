00c2b4c654c9cd49266fba55d2ca8f3949d34cb2
itrascastro <itrascastro@gmail.com>
2016-02-03 20:31:11 +0100

    Increment

--- client/leaderboard.js
+++ client/leaderboard.js
@@ -1,5 +1,5 @@
 Template.players.helpers({
-  players: Players.find()
+  players: Players.find({}, {sort:{score: -1, name: +1}})
 });
 
 Template.body.events({
@@ -8,4 +8,19 @@ Template.body.events({
     t.find('#playerName').value = '';
     e.preventDefault();
   }
+});
+
+Template.players.events({
+  'click .remove': function(e,t) {
+    Players.remove(this._id);
+    e.preventDefault();
+  },
+  'click .increment': function(e,t) {
+    // Players.update(this._id, {score: 5}); removes the document and creates a new one
+    // Players.update(this._id, {$set: {score: 5}});
+    Players.update(this._id, {$inc: {score: 5}});
+  },
+  'click .decrement': function(e,t) {
+    Players.update(this._id, {$inc: {score: -5}});
+  },
 });
